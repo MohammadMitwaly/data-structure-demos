ae5566c48bb06c62a989673f43d4702dbd6744df
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-03-08 19:42:06 +0200

    Fill circular-queue with random values

--- src/sketches/CircularQueueSketch.tsx
+++ src/sketches/CircularQueueSketch.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import p5Types from 'p5';
 import Sketch from 'react-p5';
+import CircularQueue from '../models/CircularQueue';
+import { genRandomNumber } from '../util/genRandomNumber';
 
 interface CircularQueueSketchProps {}
+
+const fillNumberQueue = (minLimit: number, maxLimit: number) => {
+    const queue = new CircularQueue<number>();
+    for (let i = 0; i < queue.maxSize; i++) {
+        queue.enQueue(genRandomNumber(minLimit, maxLimit));
+    }
+    return queue;
+};
 
 export const CircularQueueSketch: React.FC<CircularQueueSketchProps> = (
     props: CircularQueueSketchProps
 ) => {
+    const [myQueue, setMyQueue] = useState(fillNumberQueue(0, 100));
+
     const setup = (p5: p5Types, canvasParentRef: Element) => {
         p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(
             canvasParentRef
         );
+        console.log(myQueue);
     };
 
     const draw = (p5: p5Types) => {
