4b4de4c32d5da84c72875f0d1948804e1854f967
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-03-08 19:36:02 +0200

    Add sketch switching in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-// import { LinkedListSketch } from './sketches/LinkedListSketch';
+import { LinkedListSketch } from './sketches/LinkedListSketch';
+import { SketchBST } from './sketches/BinarySearchTreeSketch';
 import { StackSketch } from './sketches/StackSketch';
+import { CircularQueueSketch } from './sketches/CircularQueueSketch';
 
 const App = () => {
+    const [currentSketch, setCurrentSketch] = useState(0);
+    const handleSwitch = (n: number) => {
+        switch (n) {
+            case 0:
+                return <SketchBST />;
+            case 1:
+                return <LinkedListSketch />;
+            case 2:
+                return <StackSketch />;
+            case 3:
+                return <CircularQueueSketch />;
+        }
+    };
+
+    const updateCurrentSketch = (value: number) => {
+        setCurrentSketch(value);
+    };
+
     return (
         <>
-            {/* <SketchBST /> */}
-            {/* <LinkedListSketch /> */}
-            <StackSketch />
+            <div className="flex justify-center">
+                <button
+                    className="mr-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                    onClick={() => updateCurrentSketch(0)}
+                >
+                    Binary Search Tree
+                </button>
+                <button
+                    className="mr-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                    onClick={() => updateCurrentSketch(1)}
+                >
+                    Linked List
+                </button>
+                <button
+                    className="mr-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                    onClick={() => updateCurrentSketch(2)}
+                >
+                    Stack
+                </button>
+                <button
+                    className="mr-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                    onClick={() => updateCurrentSketch(3)}
+                >
+                    Circular Queue
+                </button>
+            </div>
+            {handleSwitch(currentSketch)}
         </>
     );
 };
