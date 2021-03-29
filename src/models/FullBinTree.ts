d7a24141ca78368b148c400b42a164a47f942d03
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-03-29 19:11:28 +0300

    Add PerfectBinTree class

--- src/models/FullBinTree.ts
+++ src/models/FullBinTree.ts
@@ -1,26 +1,17 @@
 import { BinNode } from './BinNode';
 
 export default class FullBinaryTree extends BinNode {
+    /*
+    A full Binary tree is a special type of binary tree
+    in which every parent node/internal node has either two or no children.
+    Source: https://www.programiz.com/dsa/full-binary-tree
+    */
+
     constructor(value: string | number, left?: BinNode, right?: BinNode) {
         super(value, left, right);
     }
 
     isFullBinTree(node: BinNode): boolean {
-        /*
-        // Checking tree emptiness
-        if (node == null) return true;
-
-        // Checking the children
-        if (node.leftChild == null && node.rightChild == null) return true;
-
-        if (node.leftChild != null && node.rightChild != null)
-            return (
-                isFullBinaryTree(node.leftChild) &&
-                isFullBinaryTree(node.rightChild)
-            );
-
-        return false;
-        */
         if (!node) {
             return true;
         }
---
+++ src/models/PerfectBinTree.ts
@@ -0,0 +1,48 @@
+import { rootCertificates } from 'node:tls';
+import { BinNode } from './BinNode';
+
+export default class PerfectBinTree extends BinNode {
+    /*
+    A perfect binary tree is a type of binary tree 
+    in which every internal node has exactly two child nodes and all the leaf nodes are at the same level.
+    Source: https://www.programiz.com/dsa/perfect-binary-tree
+    */
+
+    constructor(value: string | number, left?: BinNode, right?: BinNode) {
+        super(value, left, right);
+    }
+
+    getDepth(node: BinNode | undefined): number {
+        let depth = 0;
+        while (node) {
+            depth++;
+            node = node.left;
+        }
+
+        return depth;
+    }
+
+    isPerferct(
+        node: BinNode | undefined,
+        depth: number,
+        level: number
+    ): boolean {
+        if (!node) {
+            return true;
+        }
+
+        if (!node.left && !node.right) {
+            return depth === level + 1;
+        }
+
+        if (!node.left || !node.right) {
+            return false;
+        }
+
+        level++;
+        return (
+            this.isPerferct(node.left, depth, level) &&
+            this.isPerferct(node.right, depth, level)
+        );
+    }
+}
