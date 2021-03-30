e2e5580a7d826677652cb4d30bb83552fd790c33
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-03-30 17:57:41 +0300

    Add param for new tree visualization

--- src/models/BinNode.ts
+++ src/models/BinNode.ts
@@ -1,73 +1,91 @@
+import BST from './BST';
+
 export class BinNode {
-  left?: BinNode;
-  right?: BinNode;
-  parent?: BinNode;
-  value: string | number;
-  xCo: number;
-  yCo: number;
-  rightSpacing: number;
-  leftSpacing: number;
-  cumulativeRightSpacing: number;
-  cumulativeLeftSpacing: number;
-  // Constants controlling the positions of the nodes relative to one another
-  static HORIZONTALSPACING = 20; // Horizontal distance between two nodes
-  static VERTICALSPACING = 50; // Vertical distance between tow nodes
+    left?: BinNode;
+    right?: BinNode;
+    parent?: BinNode;
+    root?: BST;
+    isRightInner?: boolean;
+    value: string | number;
+    xCo: number;
+    yCo: number;
+    rightSpacing: number;
+    leftSpacing: number;
+    cumulativeRightSpacing: number;
+    cumulativeLeftSpacing: number;
+    // Constants controlling the positions of the nodes relative to one another
+    static HORIZONTALSPACING = 20; // Horizontal distance between two nodes
+    static VERTICALSPACING = 50; // Vertical distance between tow nodes
 
-  constructor(value: string | number, left?: BinNode, right?: BinNode) {
-    this.left = left;
-    this.right = right;
-    this.value = value;
-    this.xCo = 0;
-    this.yCo = 0;
-    // The horizontal space between this node and its left/right children
-    this.rightSpacing = 0;
-    this.leftSpacing = 0;
-    this.cumulativeRightSpacing = 0;
-    this.cumulativeLeftSpacing = 0;
-  }
-
-  addNodeBST(node: BinNode) {
-    if (node.value < this.value) {
-      if (!this.left) {
-        node.parent = this;
-        this.left = node;
-        this.left.xCo = this.xCo - 50;
-        this.left.yCo = this.yCo + 20;
-        node.leftSpacing =
-          this.cumulativeRightSpacing + BinNode.HORIZONTALSPACING;
-        node.cumulativeLeftSpacing =
-          node.leftSpacing + this.cumulativeLeftSpacing;
-      } else {
-        this.left.addNodeBST(node);
-      }
-    } else if (node.value > this.value) {
-      if (!this.right) {
-        node.parent = this;
-        this.right = node;
-        this.right.xCo = this.xCo + 50;
-        this.right.yCo = this.yCo + 20;
-        node.rightSpacing =
-          this.cumulativeLeftSpacing + BinNode.HORIZONTALSPACING;
-        node.cumulativeRightSpacing =
-          this.cumulativeRightSpacing + node.rightSpacing;
-      } else {
-        this.right.addNodeBST(node);
-      }
+    constructor(
+        value: string | number,
+        left?: BinNode,
+        right?: BinNode,
+        root?: BST
+    ) {
+        this.left = left;
+        this.right = right;
+        this.value = value;
+        this.xCo = 0;
+        this.yCo = 0;
+        this.root = root;
+        // The horizontal space between this node and its left/right children
+        this.rightSpacing = 0;
+        this.leftSpacing = 0;
+        this.cumulativeRightSpacing = 0;
+        this.cumulativeLeftSpacing = 0;
     }
-  }
 
-  traverseInOrder(node: BinNode | undefined, arr: number[] | string[]) {
-    if (!node) {
-      return;
+    addNodeBST(node: BinNode) {
+        if (node.value < this.value) {
+            //@ts-ignore
+            if (!this.root?.root && node.value < this.root?.root.value) {
+                node.isRightInner = false;
+            }
+            if (!this.left) {
+                node.parent = this;
+                this.left = node;
+                this.left.xCo = this.xCo - 50;
+                this.left.yCo = this.yCo + 20;
+                node.leftSpacing =
+                    this.cumulativeRightSpacing + BinNode.HORIZONTALSPACING;
+                node.cumulativeLeftSpacing =
+                    node.leftSpacing + this.cumulativeLeftSpacing;
+            } else {
+                this.left.addNodeBST(node);
+            }
+        } else if (node.value > this.value) {
+            //@ts-ignore
+            if (!this.root?.root && node.value > this.root?.root.value) {
+                node.isRightInner = true;
+            }
+            if (!this.right) {
+                node.parent = this;
+                this.right = node;
+                this.right.xCo = this.xCo + 50;
+                this.right.yCo = this.yCo + 20;
+                node.rightSpacing =
+                    this.cumulativeLeftSpacing + BinNode.HORIZONTALSPACING;
+                node.cumulativeRightSpacing =
+                    this.cumulativeRightSpacing + node.rightSpacing;
+            } else {
+                this.right.addNodeBST(node);
+            }
+        }
     }
-    this.traverseInOrder(node.left, arr);
-    //@ts-ignore
-    arr.push(node.value);
-    this.traverseInOrder(node.right, arr);
-  }
 
-  setCoordinates(x: number, y: number) {
-    this.xCo = x;
-    this.yCo = y;
-  }
+    traverseInOrder(node: BinNode | undefined, arr: number[] | string[]) {
+        if (!node) {
+            return;
+        }
+        this.traverseInOrder(node.left, arr);
+        //@ts-ignore
+        arr.push(node.value);
+        this.traverseInOrder(node.right, arr);
+    }
+
+    setCoordinates(x: number, y: number) {
+        this.xCo = x;
+        this.yCo = y;
+    }
 }
--- src/sketches/BinarySearchTreeSketch.tsx
+++ src/sketches/BinarySearchTreeSketch.tsx
@@ -1,39 +1,41 @@
-import React from "react";
-import p5Types from "p5";
-import Sketch from "react-p5";
-import { BinNode } from "../models/BinNode";
-import BST from "../models/BST";
+import React from 'react';
+import p5Types from 'p5';
+import Sketch from 'react-p5';
+import { BinNode } from '../models/BinNode';
+import BST from '../models/BST';
 
 interface BinarySearchTreeSketchProps {}
 
 export const SketchBST: React.FC<BinarySearchTreeSketchProps> = (
-  props: BinarySearchTreeSketchProps
+    props: BinarySearchTreeSketchProps
 ) => {
-  const root = new BinNode(100);
-  root.xCo = window.innerWidth / 2;
-  root.yCo = 100;
-  let rootBST = new BST(root);
-  rootBST.addNode(new BinNode(15));
-  rootBST.addNode(new BinNode(11));
-  rootBST.addNode(new BinNode(3));
-  rootBST.addNode(new BinNode(60));
-  rootBST.addNode(new BinNode(30));
-  rootBST.addNode(new BinNode(31));
-  rootBST.addNode(new BinNode(110));
-  rootBST.traverseInOrder();
-  console.log(rootBST);
-  rootBST.printParent(rootBST.root);
+    const root = new BinNode(100);
+    root.xCo = window.innerWidth / 2;
+    root.yCo = 100;
+    let rootBST = new BST(root);
+    rootBST.addNode(new BinNode(15, undefined, undefined, rootBST));
+    rootBST.addNode(new BinNode(11, undefined, undefined, rootBST));
+    rootBST.addNode(new BinNode(3, undefined, undefined, rootBST));
+    rootBST.addNode(new BinNode(60, undefined, undefined, rootBST));
+    rootBST.addNode(new BinNode(30, undefined, undefined, rootBST));
+    rootBST.addNode(new BinNode(31, undefined, undefined, rootBST));
+    rootBST.addNode(new BinNode(110, undefined, undefined, rootBST));
+    rootBST.traverseInOrder();
+    console.log(rootBST);
+    rootBST.printParent(rootBST.root);
 
-  //See annotations in JS for more information
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
-  };
+    //See annotations in JS for more information
+    const setup = (p5: p5Types, canvasParentRef: Element) => {
+        p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(
+            canvasParentRef
+        );
+    };
 
-  const draw = (p5: p5Types) => {
-    p5.background("#172121");
-    rootBST.draw(p5, rootBST.root);
-    p5.noLoop();
-  };
+    const draw = (p5: p5Types) => {
+        p5.background('#172121');
+        rootBST.draw(p5, rootBST.root);
+        p5.noLoop();
+    };
 
-  return <Sketch setup={setup} draw={draw} />;
+    return <Sketch setup={setup} draw={draw} />;
 };
